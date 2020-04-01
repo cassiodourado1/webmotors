2c6e4be832f8acb1ee11e65ec1b936b92b48bee9
Cassio Dourado <cassio.dourado@innovt.com.br>
2020-04-01 15:49:30 -0300

    Creating filters

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,28 +1,85 @@
-import React, { Component } from 'react';
-import { Content, CenterContent, VehicleList, VehicleListItem,  HeaderBar, SellButton } from './styles';
-import Header from '../../components/Header';
+import React, { Component } from "react";
+import {
+  Content,
+  CenterContent,
+  VehicleButton,
+  HeaderBar,
+  SellButton,
+  FilterBar,
+  ViewOffersButton,
+} from "./styles";
+import Header from "../../components/Header";
 
 export default class Home extends Component {
+  state = {
+    make: [],
+    model: [],
+    year: [],
+    price: [],
+    version: [],
+  }
+
   render() {
     return (
       <>
-      <Header />
-      <Content>
-        <CenterContent>
-        <HeaderBar>
-        <VehicleList>
-          <VehicleListItem>
-            <a>Carro</a>
-          </VehicleListItem>
-          <VehicleListItem>
-            <a>Moto  </a>
-          </VehicleListItem>
-        </VehicleList>
-        <SellButton>Vender meu car</SellButton>
-        </HeaderBar>
-        </CenterContent>
-      </Content>
+        <Header />
+        <Content>
+          <CenterContent>
+            <HeaderBar>
+              <ul>
+                <li>
+                  <VehicleButton>
+                    <span className="label">comprar</span>
+                    <span className="vehicle">Moto</span>
+                  </VehicleButton>
+                </li>
+                <li>
+                  <VehicleButton>
+                    <span className="label">comprar</span>
+                    <span className="vehicle moto">Moto</span>
+                  </VehicleButton>
+                </li>
+              </ul>
+              <SellButton>Vender meu car</SellButton>
+            </HeaderBar>
+            <FilterBar onSubmit={()=>{}}>
+              
+              <select name="make">
+                <option value="" selected>Marca</option> 
+                <option value="1">1</option> 
+                <option value="2">2</option>
+                <option value="3">3</option>
+              </select>
+              <select name="model">
+                <option value="" selected>Modelo</option> 
+                <option value="1">1</option> 
+                <option value="2">2</option>
+                <option value="3">3</option>
+              </select>
+              <select name="year">
+                <option value="" selected>Ano</option> 
+                <option value="1">1</option> 
+                <option value="2">2</option>
+                <option value="3">3</option>
+              </select>
+              <select name="price">
+                <option value="" selected>Preço</option> 
+                <option value="1">1</option> 
+                <option value="2">2</option>
+                <option value="3">3</option>
+              </select>
+              <select name="version">
+                <option value="" selected>Versão</option> 
+                <option value="1">1</option> 
+                <option value="2">2</option>
+                <option value="3">3</option>
+              </select>
+              <ViewOffersButton>Ver ofertas</ViewOffersButton>
+            </FilterBar>
+            
+          </CenterContent>
+        </Content>
       </>
-    )
+    );
   }
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import iconMoto from "../../assets/img/moto.svg";
+import iconCar from "../../assets/img/car.svg";
 
 export const Content = styled.div`
   width: 100%;
@@ -8,15 +10,24 @@ export const Content = styled.div`
 `;
 
 export const CenterContent = styled.div`
-  max-width: 993px;
-  width: 100%;
+  width: 993px;
 `;
 
 export const HeaderBar = styled.div`
   display: flex;
-  width: 100%;
+  max-width: 100%;
   align-items: center;
   justify-content: space-between;
+
+  ul {
+    padding: 0;
+    margin: 0;
+    display: flex;
+    border-bottom: 1px;
+    li {
+      list-style-type: none;
+    }
+  }
 `;
 
 export const SellButton = styled.button`
@@ -32,13 +43,72 @@ export const SellButton = styled.button`
   &:hover {
     background: #ff6e07;
     color: white;
-  border: 1px white solid;
+    border: 1px white solid;
   }
 `;
 
-export const VehicleList = styled.ul`
-  padding: 0;
+export const VehicleButton = styled.button`
+  padding: 16px;
+  border: none;
+  background: transparent;
+  text-transform: uppercase;
+  width: 150px;
+  border-bottom: 1px #afb7be solid;
+  cursor: pointer;
+  transition: all 0.2s;
+
+  &:hover {
+    border-bottom: 3px solid #dc3545;
+    .vehicle {
+      color: #dc3545;
+    }
+  }
+
+  .label {
+    font-size: 12px;
+    color: gray;
+    display: block;
+  }
+  .vehicle {
+    background-image: url(${iconCar});
+    background-repeat: no-repeat;
+    background-position: left center;
+    padding-left: 50px;
+    color: #afb7be;
+    font-size: 24px;
+    display: block;
+
+    &.moto {
+      background-image: url(${iconMoto});
+    }
+  }
 `;
-export const VehicleListItem = styled.ul`
-  list-style-type: none;
+
+export const FilterBar = styled.form`
+  max-width: 100%;
+  padding: 1rem 2rem;
+  background: #fff;
+  display: flex;
+  align-items: center;
+  flex-flow: row wrap;
+  border-radius: 3px;
+
+  select {
+    width: 230px;
+    height: 36px;
+    text-overflow: ""; 
+    text-indent: 0.01px;
+    margin: 10px 5px;
+  }
+`;
+
+export const ViewOffersButton = styled.button`
+  padding: 10px;
+  color: #fff;
+  font-size: 16px;
+  font-weight: 700;
+  background: #dc3545;
+  border-radius: 5px;
+  width: 200px;
+  height: 36px;
 `;
